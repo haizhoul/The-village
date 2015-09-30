aa9df585ea71f2acd6e208286e171d0f56a60288
haizhoul <haizhoul@usc.edu>
2015-09-30 12:10:27 -0700

    12.09 class meeting
    1st version

---
+++ npc/Assets/navigate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class navigate : MonoBehaviour
+{
+	Transform target;               // Reference to the player's position.
+	NavMeshAgent nav;               // Reference to the nav mesh agent.
+	
+	
+	void Awake ()
+	{
+		// Set up the references.
+		target = GameObject.FindGameObjectWithTag ("target").transform;
+
+		nav = GetComponent <NavMeshAgent> ();
+	}
+	
+	
+	void Update ()
+	{
+
+			// ... set the destination of the nav mesh agent to the player.
+			nav.SetDestination (target.position);
+		//∫Destroy (nav, 3f);
+	} 
+}
